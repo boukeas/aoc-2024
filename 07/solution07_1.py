f117cabf7678eb5c6efbe627239c91e1a5dcb322
George Boukeas <george.boukeas@canonical.com>
2024-12-07 09:11:33 +0000

    Day 07

---
+++ 07/solution07_1.py
@@ -0,0 +1,31 @@
+import sys
+
+
+def check(numbers, target) -> bool:
+    """
+    Check (recursively) if the `numbers` can be combined from left to right
+    using multiplication or addition to produce the `target`.
+    """
+    if numbers[0] > target:
+        return False
+    if len(numbers) == 1:
+        return numbers[0] == target
+    return (
+        check([numbers[0] * numbers[1]] + numbers[2:], target) or
+        check([numbers[0] + numbers[1]] + numbers[2:], target)
+    )
+
+
+# read the input line by line, breaking it down into the target and operands
+result = 0
+with open(sys.argv[1]) as file:
+    for line in file:
+        # parsing
+        target_str, operands_str = line.strip().split(":")
+        target = int(target_str)
+        operands = [int(operand) for operand in operands_str.strip().split(" ")]
+        # call `check` to compute the result
+        if check(operands, target):
+            result += target
+
+print(result)
